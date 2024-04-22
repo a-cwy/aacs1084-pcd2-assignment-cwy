8fbc29eee284a783c138db57bb99c2f7dec51809
Cranageist <cranageist@gmail.com>
2024-04-23 00:14:57 +0800

    Add Validation Functions

--- aacs1084-pcd2-assignment-cwy/Util.c
+++ aacs1084-pcd2-assignment-cwy/Util.c
@@ -4,10 +4,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 #pragma warning(disable:4996)
 
-int displayMenu(char *menuOptions[], int optionsCount) {
+//
+// Menu
+//
+
+int displayMenu(const char *menuOptions[], int optionsCount) {
 	//Generate and print menu
 	printf("==================================================\n");
 	printf("0\t| Exit/Back\n");
@@ -29,7 +35,7 @@ int displayMenu(char *menuOptions[], int optionsCount) {
 	return(choice);
 }
 
-int handleLogin(char *loginType[]) {
+int handleLogin(char loginType) {
 	// Variables for login information
 	char loginID[6], loginPassword[40];
 	
@@ -37,20 +43,105 @@ int handleLogin(char *loginType[]) {
 	printf("Login :\n");
 	printf("\tID       : ");
 	rewind(stdin);
-	fgets(&loginID, 5, stdin);
+	fgets(loginID, 5, stdin);
 
 	printf("\tPassword : ");
 	rewind(stdin);
-	fgets(&loginPassword, 39, stdin);
+	fgets(loginPassword, 39, stdin);
 
 	// Function call to validate login
 	int code = 0;
-	if (strcmp(loginType, "staff") == 0) {
-		code = validateStaffLogin(&loginID, &loginPassword);
+	if (loginType == 's')
+	{
+		code = validateStaffLogin(loginID, loginPassword);
 	}
 	else {
 		// code = validateMemberLogin(&loginID, &loginPassword);
 	}
 
 	return(code);
+}
+
+
+//
+// Input Valdiation
+//
+
+bool validateStaffID(const char* staffID) {
+	if (strlen(staffID) < 5) return(false);
+	if (staffID[0] != 'S') return(false);
+
+	for (int i = 1; i < 5; i++)
+	{
+		if (!isdigit(staffID[i])) return(false);
+	}
+
+	return(true);
+}
+
+bool validateIC(const char* IC) {
+	if (strlen(IC) < 14) return(false);
+
+	for (int i = 0; i < 14; i++)
+	{
+		if (i == 6 || i == 9) {
+			if (IC[i] != '-') return(false);
+			continue;
+		}
+
+		if (!isdigit(IC[i])) return(false);
+	}
+
+	return(true);
+}
+
+bool validateGender(const char* gender) {
+	if (*gender != 'M' && *gender != 'F') return(false);
+	return(true);
+}
+
+bool validatePhoneNumber(const char* phoneNumber) {
+	if (strlen(phoneNumber) < 11) return(false);
+
+	for (int i = 0; i < strlen(phoneNumber) - 1; i++)
+	{
+		if (i == 3) {
+			if (phoneNumber[i] != '-') return(false);
+			continue;
+		}
+
+		if (!isdigit(phoneNumber[i])) return(false);
+	}
+
+	return(true);
+}
+
+bool validateRecoveryPIN(const char* PIN) {
+	if (strlen(PIN) < 6) return(false);
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (!isdigit(PIN[0])) return(false);
+	}
+
+	return(true);
+}
+
+bool validateEmail(const char* email) {
+	bool hasAt = false;
+	bool hasDot = false;
+	int atLoc = 0, dotLoc = 0;
+
+	for (int i = 0; i < strlen(email); i++)
+	{
+		if (email[i] == '@') { hasAt = true; atLoc = i; }
+		if (email[i] == '.') { hasDot = true; dotLoc = i; }
+		if (email[i] == ' ') return(false);
+	}
+
+	return(hasAt && hasDot && (atLoc < dotLoc - 1) && (atLoc != 0) && (dotLoc != strlen(email) - 1));
+}
+
+bool validateEmploymentType(const char* employmentType) {
+	return(strcmp(employmentType, "FT") == 0 || strcmp(employmentType, "PT") == 0);
 }
--- aacs1084-pcd2-assignment-cwy/Util.h
+++ aacs1084-pcd2-assignment-cwy/Util.h
@@ -1,9 +1,35 @@
 #pragma once
 
-//Menu Utilities
+#include <stdbool.h>
+
+//Menu
 
 // Displays a menu with options based on parameter. Returns integer for selected menu option.
-int displayMenu(char *menuOptions[], int optionsCount);
+int displayMenu(const char *menuOptions[], int optionsCount);
 
 // Handles login for "staff" or "member".
-int handleLogin(char* loginType[]);
+int handleLogin(char loginType);
+
+
+// Input Validation
+
+// StaffID follows format S1234
+bool validateStaffID(const char* staffID);
+
+// IC number follows format 123456-12-1234
+bool validateIC(const char* IC);
+
+// Gender is either M or F
+bool validateGender(const char* gender);
+
+// Phone number follows format 012-34567890
+bool validatePhoneNumber(const char* phoneNumber);
+
+// PIN folows format 123456
+bool validateRecoveryPIN(const char* PIN);
+
+// Employment type is either (FT/PT)
+bool validateEmploymentType(const char* employmentType);
+
+// Email follows format abc@email.com
+bool validateEmail(const char* email);
