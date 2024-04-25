c7f5499fc410df9304c46f24967986a0da13cf13
Cranageist <cranageist@gmail.com>
2024-04-25 18:11:13 +0800

    Change Menu Layout

--- aacs1084-pcd2-assignment-cwy/Source.c
+++ aacs1084-pcd2-assignment-cwy/Source.c
@@ -7,48 +7,27 @@
 
 #pragma warning(disable:4996)
 
-#define MENU_OPTION_SIZE 4
-const char *MAIN_MENU_OPTIONS[MENU_OPTION_SIZE] = { "View Staff Info", "Create Staff Account", "Staff Login", "Add Train"};
+#define MAIN_MENU_OPTION_SIZE 2
+const char *MAIN_MENU_OPTIONS[MAIN_MENU_OPTION_SIZE] = { "Staff Login", "Add Train"};
 
 int main(void) {
-
 	int menuChoice;
 	
 	while(1)
 	{
+		system("cls");
 		int loginRetVal = 0;
-		menuChoice = displayMenu(MAIN_MENU_OPTIONS, MENU_OPTION_SIZE);
+		menuChoice = displayMenu(MAIN_MENU_OPTIONS, MAIN_MENU_OPTION_SIZE);
 		Staff tempStaff = { 0 };
 
 		switch (menuChoice) {
 		case 0: // Exit
 			return(0);
 			break;
-		case 1: //
-			viewStaffInfo();
+		case 1: // Staff Login
+			staffMenu();
 			break;
-		case 2: //
-			createStaff(&tempStaff);
-			break;
-		case 3: // Staff Login
-			loginRetVal = handleLogin('s');
-
-			switch (loginRetVal)
-			{
-			case 1: // Login unsuccessful
-			case 2: // File not found
-				printf("\n\n\nLogin failed. Please try again.%d\n\n", loginRetVal);
-				continue;
-				break;
-			case 0: // Login accepted
-				printf("\n\n\nLogin success.\n\n");
-				break;
-			default:
-				break;
-			}
-
-			break;
-		case 4:
+		case 2:
 			addTrain();
 			break;
 		default:
